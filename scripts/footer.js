ea6072b87dd08ae3e92d9ede21746d1ea7934873
camilo0120castro <camilo0120castrov@gmail.com>
2026-02-28 18:41:48 -0500

    optimized code

--- scripts/footer.js
+++
@@ -1,6 +0,0 @@
-const copyrightElement = document.getElementById("copyright-year");
-if (copyrightElement) {
-    const currentYear = new Date().getFullYear();
-    copyrightElement.innerHTML = copyrightElement.innerHTML.replace("[YEAR]", currentYear);
-}
-
